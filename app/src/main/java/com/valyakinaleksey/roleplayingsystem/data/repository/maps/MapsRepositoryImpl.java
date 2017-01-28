cdb7dd84ed668b572785a935f663c7cda7642cb4
xzaleksey <xzaleksey@gmail.com>
2017-01-28 16:06:33 +0300

    Added image compression.

--- app/src/main/java/com/valyakinaleksey/roleplayingsystem/data/repository/maps/MapsRepositoryImpl.java
+++ app/src/main/java/com/valyakinaleksey/roleplayingsystem/data/repository/maps/MapsRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.valyakinaleksey.roleplayingsystem.data.repository.maps;
 
+import android.graphics.Bitmap;
 import com.kbeanie.multipicker.utils.FileUtils;
 import com.valyakinaleksey.roleplayingsystem.core.utils.RxTransformers;
+import com.valyakinaleksey.roleplayingsystem.di.app.RpsApp;
 import com.valyakinaleksey.roleplayingsystem.utils.FireBaseUtils;
 import com.valyakinaleksey.roleplayingsystem.utils.PathManager;
 import com.valyakinaleksey.roleplayingsystem.utils.StringUtils;
+import id.zelory.compressor.Compressor;
 import java.io.File;
 import java.io.IOException;
 import rx.Observable;
@@ -18,18 +21,14 @@ public class MapsRepositoryImpl implements MapsRepository {
   }
 
   @Override public Observable<File> createLocalFileCopy(String gameId, File file) {
+
     return Observable.just(file).compose(RxTransformers.applyIoSchedulers()).map(file1 -> {
-      File newFile = new File(pathManager.getCachePath()
+      String newDirectory = pathManager.getCachePath()
           .concat(StringUtils.formatWithSlashes(gameId))
-          .concat(FireBaseUtils.GAME_MAPS)
-          .concat("/")
-          .concat(file.getName()));
-      try {
-        FileUtils.copyFile(file, newFile);
-      } catch (IOException e) {
-        Observable.error(e);
-      }
-      return newFile;
+          .concat(FireBaseUtils.GAME_MAPS);
+      Compressor compressor =
+          new Compressor.Builder(RpsApp.app()).setDestinationDirectoryPath(newDirectory).build();
+      return compressor.compressToFile(file);
     });
   }
 }
