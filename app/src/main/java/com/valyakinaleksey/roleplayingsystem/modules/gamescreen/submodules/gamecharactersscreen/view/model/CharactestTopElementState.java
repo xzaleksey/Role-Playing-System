ea7bac114f5a9351595bc465b65830eac3cb28aa
xzaleksey <xzaleksey@gmail.com>
2018-03-17 16:48:10 +0300

    In progress of top bar in characters

---
+++ app/src/main/java/com/valyakinaleksey/roleplayingsystem/modules/gamescreen/submodules/gamecharactersscreen/view/model/CharactestTopElementState.java
@@ -0,0 +1,6 @@
+package com.valyakinaleksey.roleplayingsystem.modules.gamescreen.submodules.gamecharactersscreen.view.model;
+
+public enum CharactestTopElementState {
+    PLAYER_NO_CHARACTER,
+    PLAYER_WITH_CHARACTER;
+}
